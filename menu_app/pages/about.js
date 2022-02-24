0165bce77153a83d595d0078c89846165b07987a
jimmym620 <jimmym620@gmail.com>
2022-02-24 22:54:07 +0000

    base components

---
+++ menu_app/pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return(
+        <div>
+            <h1>About us</h1>
+            <p>This restaurant was opened in 1969 by John Smith</p>
+        </div>
+    );
+};
+
+export default About;
--- menu_app/pages/index.js
+++ menu_app/pages/index.js
@@ -1,11 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Steakhouse Menu</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Pending Restaurant</h1>
+            <h2>Daily Specials</h2>
+            <Link href="/about">
+                <a>Chicken Special</a>
+            </Link>
+        </div>
+    );
 }
