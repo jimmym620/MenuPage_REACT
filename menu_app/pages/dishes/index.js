cb042a584ed52d49472d3e1db95ac6f18729cee1
jimmym620 <jimmym620@gmail.com>
2022-02-25 14:06:11 +0000

    added dish images

--- menu_app/pages/dishes/index.js
+++ menu_app/pages/dishes/index.js
@@ -1,18 +1,44 @@
 import Link from "next/link";
 import dishes from "/data/dish-data.json";
+import Image from "next/image";
 
 const Dishes = () => {
     return (
-        <div>
-            <h1>Food</h1>
-            {dishes.food.map((item) => {
-                return (
-                    <article key={item.id}>
-                        <h3 className="menu-item-name">{item.name}</h3>
-                        <h4>{`$${item.price}`}</h4>
-                    </article>
-                );
-            })}
+        <div className="menu-list">
+            <section>
+                <h1>Food</h1>
+                {dishes.food.map((item) => {
+                    return (
+                        <article key={item.id}>
+                            <h3 className="menu-item-name">{item.name}</h3>
+                            <h4>{`$${item.price}`}</h4>
+                            <Image
+                                src={item.thumbnail}
+                                alt={item.altText}
+                                width={420}
+                                height={270}
+                            />
+                        </article>
+                    );
+                })}
+            </section>
+            <section>
+                <h1>Drinks</h1>
+                {dishes.drinks.map((item) => {
+                    return (
+                        <article key={item.id}>
+                            <h3 className="menu-item-name">{item.name}</h3>
+                            <h4>{`$${item.price}`}</h4>
+                            <Image
+                                src={item.thumbnail}
+                                alt={item.altText}
+                                width={420}
+                                height={270}
+                            />
+                        </article>
+                    );
+                })}
+            </section>
         </div>
     );
 };
