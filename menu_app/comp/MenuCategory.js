9a898dc344e2151d85021d3cdd6beb96a3282f39
jimmym620 <jimmym620@gmail.com>
2023-04-05 22:06:00 +0100

    menu items styling + footer at bottom

--- menu_app/comp/MenuCategory.js
+++ menu_app/comp/MenuCategory.js
@@ -1,9 +1,9 @@
 import dishes from "/data/dish-data.json";
 import Link from "next/link";
 
-const Dishes = ({cat}) => {
+const Dishes = ({ cat }) => {
     return (
-        <div className={`categories ${cat}-pastel` }>
+        <div className={`categories ${cat}-pastel`}>
             <h1>{cat}</h1>
             {dishes.menu.map((item) => {
                 if (item.category.includes(cat)) {
@@ -11,12 +11,10 @@ const Dishes = ({cat}) => {
                         <article key={item.id} className="menu-item">
                             <Link href={"/dishes/" + item.id}>
                                 <a>
-                                    <h3>
-                                        {item.name}
-                                    </h3>
+                                    <h3>{item.name}</h3>
                                 </a>
                             </Link>
-                            <h4>{`$${item.price}`}</h4>
+                            <h3>{`$${item.price}`}</h3>
                         </article>
                     );
                 }
